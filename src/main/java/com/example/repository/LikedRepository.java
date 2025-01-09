835a693ef2d00095def795f6798caf5865fee6bc
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2025-01-09 19:52:33 +0100

    add first custom request for liked

--- src/main/java/com/example/repository/LikedRepository.java
+++ src/main/java/com/example/repository/LikedRepository.java
@@ -2,7 +2,15 @@
 package com.example.repository;
 
 import com.example.model.Article;
+import com.example.model.Liked;
+import com.example.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LikedRepository  extends CrudRepository<Article, Integer> {
+import java.util.List;
+
+public interface LikedRepository  extends CrudRepository<Liked, Integer> {
+    Liked findByArticleAndUser(Article article, User user);
+    void updateLikedByArticleAndUser(Article article, User user, boolean liked);
+    List<Liked> findByArticle(Article article);
+    int countByArticleAndLiked(Article article, boolean liked);
 }
