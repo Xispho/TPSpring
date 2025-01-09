fed07e9baf8f238574e030f9212ca415015d9186
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2025-01-09 20:12:06 +0100

    update liked status by article and user

--- src/main/java/com/example/repository/LikedRepository.java
+++ src/main/java/com/example/repository/LikedRepository.java
@@ -4,13 +4,23 @@ package com.example.repository;
 import com.example.model.Article;
 import com.example.model.Liked;
 import com.example.model.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface LikedRepository  extends CrudRepository<Liked, Integer> {
     Liked findByArticleAndUser(Article article, User user);
-    void updateLikedByArticleAndUser(Article article, User user, boolean liked);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Liked l SET l.liked = :liked WHERE l.article = :article AND l.user = :user")
+    void updateLikedByArticleAndUser(@Param("article") Article article, @Param("user") User user, @Param("liked") boolean liked);
+
     List<Liked> findByArticle(Article article);
+
     int countByArticleAndLiked(Article article, boolean liked);
 }
